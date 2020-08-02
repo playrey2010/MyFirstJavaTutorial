28c68cccaab22fe5b28246ba0ef0e25dd7da55a8
playrey2010 <playrey2010@gmail.com>
2020-08-02 12:29:25 -0400

    added my age

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         String miNombre = "Rey", miApellido = "Aponte";
         int miEdad = 99;
 //        miNombre = "Rey";
-        System.out.println("My full name is " + miNombre + " " + miApellido + miEdad);
+        System.out.println("My full name is " + miNombre + " " + miApellido + " y mi edad es " + miEdad);
 
     }
 }
