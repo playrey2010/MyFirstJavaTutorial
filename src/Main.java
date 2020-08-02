dbfe29d30c75097d448902d050902b51fa53a933
playrey2010 <playrey2010@gmail.com>
2020-08-02 12:12:58 -0400

    My first push

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        String miNombre = "Rey", miApellido = "Aponte";
+        int miEdad = 99;
+//        miNombre = "Rey";
+        System.out.println("My full name is " + miNombre + " " + miApellido + miEdad);
+
+    }
+}
